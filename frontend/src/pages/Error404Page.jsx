e37b8d5ba1f94aa64091d6cb6fa87517980264c6
Lee Siang Yin <siangyin916@gmail.com>
2022-02-21 16:05:05 +0800

    error page

--- frontend/src/pages/Error404Page.jsx
+++ frontend/src/pages/Error404Page.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+
+import error404img from "../assets/error404.jpg";
 
 const Error404Page = () => {
-  return (
-    <div>Error404Page</div>
-  )
-}
+	return (
+		<div className="text-center">
+			<img
+				src={error404img}
+				alt={error404img}
+				className="mx-auto d-block"
+				height="300px"
+			/>
+			<h2>Ooops... sorry, there was an error!</h2>
+			<button type="button" class="btn btn-primary">
+				return to homepage
+			</button>
+		</div>
+	);
+};
 
-export default Error404Page
+export default Error404Page;
--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 const ProductListPage = () => {
-  return (
-    <div>ProductListPage</div>
-  )
-}
+	const [productDB, setProductDB] = useState();
 
-export default ProductListPage
+	useEffect(() => {
+		const getAllProductsAPI = async () => {
+			try {
+				const res = await axios(
+					"https://greenlab-be.herokuapp.com/api/v1/products",
+					{
+						method: "GET",
+						mode: "no-cors",
+						headers: {
+							"Access-Control-Allow-Origin": "*",
+							"Content-Type": "application/json",
+						},
+						withCredentials: true,
+						credentials: "same-origin",
+					}
+				);
+				console.error(res.data.products);
+				setProductDB(res.data.products);
+				return res;
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		getAllProductsAPI();
+	}, [productDB]);
+
+	return (
+		<div>
+			<h3 className="breadcrumb-item text-muted active">Product List</h3>
+			<div>
+				{productDB &&
+					productDB.map((item) => {
+						return <h6>{item.name}</h6>;
+					})}
+			</div>
+		</div>
+	);
+};
+
+export default ProductListPage;
