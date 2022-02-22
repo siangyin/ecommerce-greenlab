0736eb765294fc6b58e67b0bdc06c68b60136e6b
Lee Siang Yin <siangyin916@gmail.com>
2022-02-23 00:27:25 +0800

    getsingleitem data

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -3,26 +3,44 @@ import { useContext, useEffect } from "react";
 import { LoginContext, userIDContext } from "../helper/context";
 import { BE_URL } from "../helper/const";
 
-const ProductCard = ({ name, price, image, url, id }) => {
+const ProductCard = ({
+	name,
+	price,
+	image,
+	url,
+	id,
+	numOfReviews,
+	averageRating,
+}) => {
+	// for add to cart
 	//http://localhost:5000/api/carts?productId=123&qty=5&userId=321
 
 	const { userID, setUserID } = useContext(userIDContext);
 
-	console.log(userID);
+	// console.log(userID);
+
+	function starRating(num) {
+		let rate = num;
+		let star = "";
+		for (let i = 0; i < rate; i++) {
+			star += "⭐️";
+		}
+		return star;
+	}
 
 	return (
 		<div className="card">
 			<div className="card-body">
-				<Link to={url} className="card-link">
+				<Link to={url}>
 					<img src={image} alt={name} />
-					<h4 className="card-title">{name}</h4>
-					<h6 className="card-subtitle mb-2 text-muted">{price}</h6>
-					<p className="card-text">
-						<i className="fa-solid fa-star"></i>
-					</p>
-					Card link
 				</Link>
-				<button className="card-link">Add to cart</button>
+				<h4 className="card-title">{name}</h4>
+				<h6 className="card-subtitle mb-2 text-muted">{price}</h6>
+				<p className="card-text">{starRating(averageRating)} </p>
+				Card link
+				<button type="button" className="btn btn-primary">
+					add to cart
+				</button>
 			</div>
 		</div>
 	);
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BE_URL } from "../helper/const";
 
 const ProductPage = () => {
-  return (
-    <div>ProductPage</div>
-  )
-}
+	const { id } = useParams();
+	const [singleProdDB, setSingleProdDB] = useState();
 
-export default ProductPage
+	console.log(id);
+
+	useEffect(() => {
+		axios
+			.get(`${BE_URL}/products/${id}`)
+			.then((res) => {
+				console.log(res.data);
+				if (res.data.status === "OK") {
+					setSingleProdDB(res.data.data);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
+	return (
+		<div className="card" style={{ width: "18rem" }}>
+			<img
+				src={singleProdDB.image}
+				className="card-img-top"
+				alt={singleProdDB.name}
+			/>
+			<div className="card-body">
+				<h5 className="card-title text-primary">{singleProdDB.name}</h5>
+				<p className="card-text">{singleProdDB.description}</p>
+			</div>
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item">
+					Reviews: {singleProdDB.numOfReviews}
+				</li>
+				<li className="list-group-item">A second item</li>
+				<li className="list-group-item">A third item</li>
+			</ul>
+			<div className="card-body">
+				<a href="#" className="card-link">
+					Card link
+				</a>
+				<a href="#" className="card-link">
+					Another link
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default ProductPage;
